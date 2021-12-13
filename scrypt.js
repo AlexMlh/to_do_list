7d5f5b3ef2f5aba8ce7fa710bfc719283c73b6da
San4o1313 <malagan31@gmail.com>
2021-12-13 13:27:05 +0200

    new features

--- scrypt.js
+++ scrypt.js
@@ -1,47 +1,75 @@
-var button = document.getElementById("button");
-var inpt = document.querySelector("input");
-var ul = document.getElementById("myList");
-var ulItems =document.querySelectorAll("li button");
-var li =document.querySelectorAll("li");
+let button = document.getElementById("button");
+let inpt = document.querySelector("input");
+let ul = document.getElementById("myList");
+let ulItems =document.querySelectorAll("li button");
+let li =document.querySelectorAll("li");
+let delBtn = document.getElementById("del");
 
-for (i=0; i< li.length; i++) {
-  li[i].querySelector("h4").addEventListener("click", boldFunction)
+if(localStorage.length != 0) {
+  storege =JSON.parse(localStorage.getItem("storage"));
+  console.log(storege)
+;  storege.forEach(i => addLi(Object.keys(i)[0], i[Object.keys(i)[0]]));
 }
-function boldFunction() {
-  this.classList.toggle("bold");
-}   
-
 
 button.addEventListener("click" ,function() {
   if (inpt.value !=="") {
-    addLi();
+    addLi(inpt.value);
   }
 })
 
 inpt.addEventListener("keypress" ,function(event) {
   if (inpt.value !=="" && event.key === "Enter"  ) {
-  addLi(); 
+    addLi(inpt.value); 
   }
 })
 
-for (i=0; i< ulItems.length; i++) {
-  ulItems[i].addEventListener("click", myfunction );
+
+window.onunload =  function() {
+  localStorage.clear();
+  let li =document.querySelectorAll("li");
+  if(li.length == 0) return;
+  let str = [];
+  li.forEach(function(item) {
+    text = item.querySelector("h4").textContent;
+    str.push({[text] : item.querySelector("h4").classList.contains("crossLine")});
+  })
+  localStorage.setItem("storage",JSON.stringify(str));
 }
 
-function myfunction() {
+function removeItem() {
   this.parentNode.remove();
+  if(delBtn.style.visibility == "visible" && document.querySelectorAll("ul li").length < 3) {
+    delBtn.style.visibility = "hidden";
+  } 
 }
 
-function addLi() {
+function crossLine() {
+  this.classList.toggle("crossLine");
+}   
+
+function clearFunc() {
+  ul.innerHTML = "";  
+  delBtn.style.visibility = "hidden";
+  console.log("clear")
+}
+
+function addLi(itm, cross = false) {
   li = document.createElement("li");
-  let item = document.createElement("h4");
-  item.textContent = inpt.value;
-  li.appendChild(item);
+  let textItem = document.createElement("h4");
+  textItem.addEventListener("click", crossLine);
+  if(cross) {textItem.classList.add('crossLine')};
+  textItem.textContent = itm;
+  li.appendChild(textItem);
   btn = document.createElement("button");
-  btn.appendChild(document.createTextNode("delite"));
+  let rmv = document.createTextNode("X");
+  rmv.addEventListener("click", removeItem)
+  btn.appendChild(rmv);
   li.appendChild(btn);
   ul.appendChild(li);
-  btn.addEventListener("click", myfunction);
-  item.addEventListener("click", boldFunction);
+  btn.addEventListener("click", removeItem);
+  textItem.addEventListener("click", crossLine);
   inpt.value = '';
+  if(delBtn.style.visibility == "hidden" && document.querySelectorAll("ul li").length > 2) {
+    delBtn.style.visibility = "visible";
+  } 
 } 
